78bd85f282b96742d6aefd0ecc69ed7dd0d73fdf
QuQ-bb <quqsue@gmail.com>
2022-10-25 18:29:33 +0900

    흠냥

--- src/main/java/com/iu/home/member/MemberService.java
+++ src/main/java/com/iu/home/member/MemberService.java
@@ -2,8 +2,10 @@ package com.iu.home.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class MemberService {
 	
 	@Autowired
@@ -12,6 +14,10 @@ public class MemberService {
 	public int setJoin(MemberVO memberVO)throws Exception{
 		
 		int result = memberMapper.setJoin(memberVO);
+		if(result <1) {
+			throw new Exception();
+		}
+		
 			
 			memberMapper.setRole(memberVO);
 		
